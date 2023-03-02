3cd5bebb23ea52af1d3a3fbb620543230ffc8d66
GabbAlves <alucar@gmail.com>
2023-03-02 06:04:30 -0300

    criação do dto

---
+++ backend/src/main/java/com/GOATstore/backend/controller/PessoaClienteController.java
@@ -0,0 +1,39 @@
+package com.GOATstore.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.GOATstore.backend.entity.Pessoa;
+import com.GOATstore.backend.service.PessoaService;
+
+import dto.PessoaClienteRequestDTO;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class PessoaClienteController {
+      
+    @Autowired
+    private PessoaService pessoaService;
+
+    
+    @PostMapping("/")
+        public Pessoa inserir(@RequestBody PessoaClienteRequestDTO pessoaClienteRequestDTO){
+           Pessoa pessoa = new PessoaClienteRequestDTO().converter(pessoaClienteRequestDTO);
+            return pessoaService.inserir(pessoa);
+        }
+    
+
+
+
+
+}
