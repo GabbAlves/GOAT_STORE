3472034eeef583604e7e09e629cda9ef493877ff
GabbAlves <101905350+GabbAlves@users.noreply.github.com>
2023-03-03 21:28:51 -0300

    Back crud estado (#2)
    * Criação Controller Estado
    
    * Criação de diversos cruds, entity,service...
    
    * finalizado repository, service, controller e entitys
    
    * criado permissao pessoas e imagens de produtos
    
    * criação do dto
    
    * Permissao cliente e metodo criado
    
    * permissao quebrado, excluir arrumado e testado
    
    ---------
    
    Co-authored-by: Gabriel Alves <gabrielalves@hubert.com.br>
    Co-authored-by: GabbAlves <alucar@gmail.com>

---
+++ backend/src/main/java/com/GOATstore/backend/service/PessoaClienteService.java
@@ -0,0 +1,34 @@
+// package com.GOATstore.backend.service;
+
+// import java.util.Date;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+
+// import com.GOATstore.backend.entity.Pessoa;
+// import com.GOATstore.backend.repository.PessoaClienteRepository;
+
+
+// import dto.PessoaClienteRequestDTO;
+
+// @Service
+// public class PessoaClienteService{
+
+//     @Autowired
+//     private PessoaClienteRepository pessoaRepository;
+    
+//     @Autowired
+//     private PermissaoPessoaService permissaoPessoaService;
+
+//     public Pessoa inserir(PessoaClienteRequestDTO pessoaClienteRequestDTO){
+//         Pessoa pessoa = new PessoaClienteRequestDTO().converter(pessoaClienteRequestDTO);  
+        
+//         pessoa.setDataCriacao(new Date());
+//         Pessoa pessoaNovo = pessoaRepository.saveAndFlush(pessoa);
+//         permissaoPessoaService.vincularPessoaPermissaoCliente(pessoaNovo);
+//         return pessoaNovo;
+//     }
+
+
+
+// }
